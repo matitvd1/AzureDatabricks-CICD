fb3529433b2eca78c3845b31526a7893e85caedd
Matias Vargas <matiasvargas598@gmail.com>
2022-10-10 11:23:43 -0300

    databricks commit

---
+++ LoggingApp.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %fs ls dbfs:/cluster-logs/
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/cluster-logs/1009-131237-yyc1f7og/driver/
+
+# COMMAND ----------
+
+# MAGIC %fs head dbfs:/cluster-logs/1009-131237-yyc1f7og/driver/log4j-active.log
